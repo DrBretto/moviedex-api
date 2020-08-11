c5f9c46f8f2802217918798abd10e7c7b2c7b009
Brett Westerlund <drbretto82@gmail.com>
2020-08-11 13:15:20 -0400

    added .env

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-const express = require('express')
-const morgan = require('morgan')
+//de9ed7c0-dbf4-11ea-87d0-0242ac130003
 
-const app = express()
+require('dotenv').config()
 
-app.use(morgan('dev'))
+const validTypes = [
+  `Bug`,
+  `Dark`,
+    `Dragon`,
+    `Electric`,
+    `Fairy`,
+    `Fighting`,
+    `Fire`,
+    `Flying`,
+    `Ghost`,
+    `Grass`,
+    `Ground`,
+    `Ice`,
+    `Normal`,
+    `Poison`,
+    `Psychic`,
+    `Rock`,
+    `Steel`,
+    `Water`,
+  ];
 
-app.use((req, res) => {
-  res.send('Hello, world!')
-})
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-const PORT = 8000
+app.use(morgan("dev"));
+
+console.log(process.env.API_TOKEN)
+
+function handleGetTypes(req, res) {}
+
+app.get("/types", handleGetTypes);
+
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});

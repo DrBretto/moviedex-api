dacb47de68ca429f2486ec79094920c4e8217f64
Brett Westerlund <drbretto82@gmail.com>
2020-08-12 09:53:30 -0400

    intial commmit

--- server.js
+++ server.js
@@ -1,62 +1,52 @@
-//de9ed7c0-dbf4-11ea-87d0-0242ac130003
-
 require("dotenv").config();
-
-const validTypes = [
-  `Bug`,
-  `Dark`,
-  `Dragon`,
-  `Electric`,
-  `Fairy`,
-  `Fighting`,
-  `Fire`,
-  `Flying`,
-  `Ghost`,
-  `Grass`,
-  `Ground`,
-  `Ice`,
-  `Normal`,
-  `Poison`,
-  `Psychic`,
-  `Rock`,
-  `Steel`,
-  `Water`,
-];
-
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+
+const MOVIES = require("./movies-data.json");
 
 app.use(morgan("dev"));
+app.use(validateBearerToken);
+app.use(cors());
+app.use(helmet());
 
-app.use(function validateBearerToken(req, res, next) {
+app.get("/movie", handleGetMovie);
+
+function validateBearerToken(req, res, next) {
   console.log("validate bearer token middleware");
-
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-
+  console.log("validateBearerToken -> authToken", authToken);
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
-
-  // move to the next middleware
   next();
-});
-
-console.log(process.env.API_TOKEN);
-
-function handleGetTypes(req, res) {}
-
-app.get("/types", handleGetTypes);
-
-function handleGetPokemon(req, res) {
-  res.send("Hello, Pokemon!");
 }
 
-app.get("/pokemon", handleGetPokemon);
+function handleGetMovie(req, res) {
+  let response = MOVIES;
 
-function handleGetTypes(req, res) {
-  res.json(validTypes);
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      (movie) => parseFloat(movie.avg_vote) >= parseFloat(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
 }
 
 const PORT = 8000;
